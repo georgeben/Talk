3c82439d996d16cd396700728388f099d65e1ae0
georgeben <msdcconnect@gmail.com>
2019-07-05 07:35:28 +0100

    Setup routes

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueChatScroll from 'vue-chat-scroll'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/loading.css'
+import './assets/css/loading-btn.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueChatScroll)
+Vue.use(BootstrapVue)
 
 new Vue({
   router,

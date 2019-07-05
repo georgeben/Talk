1ac2e6cd599dfe80e1586ffc2ca7a723d5b312b2
georgeben <msdcconnect@gmail.com>
2019-07-05 11:10:00 +0100

    Create login form

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="login">
+    <b-jumbotron  header="Talk"
+                  lead="Powered by Chatkit SDK and Bootstrap-Vue"
+                  bg-variant="info"
+                  text-variant="white">
+    </b-jumbotron>
+    <b-container>
+      <b-row>
+        <b-col lg="4" md="3"></b-col>
+        <b-col lg="4" md="6">
+          <LoginForm />
+        </b-col>
+        <b-col lg="4" md="3"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm'
+
+export default {
+    name: 'login',
+    components: {
+        LoginForm
+    }
+}
+</script>
+
